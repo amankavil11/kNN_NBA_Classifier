f23875e819c0449872572cc1dba56bcaa82b9a87
Abe <abe.m.mankavil@gmail.com>
2023-03-13 03:55:16 -0400

    Raise exception for missing target_col

--- load_stats_func.py
+++ load_stats_func.py
@@ -3,10 +3,9 @@ import pandas as pd
 def load_NBA_stats(return_X_y=False, target_col=None, split_test=False, split_test_elements=None):
     df_NBA_Stats = pd.read_csv('NBA_Player_Stats.csv')
 
-    
     if return_X_y:
-        # if not target_col:
-        #     raise Exception('Target column along which axis to split was not provided.')
+        if target_col is None:
+            raise Exception('Target column along which axis to split was not provided.')
         df_X, s_y = df_NBA_Stats.drop([target_col],axis=1), df_NBA_Stats[target_col].reset_index(drop=True)
         df_NBA_Stats = (df_X, s_y)
 
