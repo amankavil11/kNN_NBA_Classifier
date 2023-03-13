25ce39526c87b7381b18f8d2ffa8e151b5da6131
Abe <abe.m.mankavil@gmail.com>
2023-03-12 23:14:15 -0400

    Move load_NBA_stats to seperate .py file

---
+++ load_stats_func.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def load_NBA_stats(return_X_y=False, target_col=None, split_test=False, split_test_elements=None):
+    df_NBA_Stats = pd.read_csv('NBA_Player_Stats.csv')
+
+    
+    if return_X_y:
+        # if not target_col:
+        #     raise Exception('Target column along which axis to split was not provided.')
+        df_X, s_y = df_NBA_Stats.drop([target_col],axis=1), df_NBA_Stats[target_col].reset_index(drop=True)
+        df_NBA_Stats = (df_X, s_y)
+
+    if split_test:
+        splits = {'train':[], 'test':[]}
+        if not return_X_y:
+            df_NBA_Stats = (df_NBA_Stats,)
+        for vectors in df_NBA_Stats:
+            df_train, df_test = vectors.iloc[:-1*split_test_elements], vectors.iloc[-1*split_test_elements:].reset_index(drop=True)
+            splits['train'].append(df_train)
+            splits['test'].append(df_test)
+        
+        df_NBA_Stats = tuple(splits['train'] + splits['test'])
+
+    return df_NBA_Stats
